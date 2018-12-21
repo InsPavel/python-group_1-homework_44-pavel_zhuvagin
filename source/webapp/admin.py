e8968150b2440e51339460f993c82051cd2259ef
pavel <zhpv07@gmail.com>
2018-12-20 20:47:07 -0500

    Regitered models in Admin

--- source/webapp/admin.py
+++ source/webapp/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
+from django.contrib.auth.models import User
+from django.contrib.auth.admin import UserAdmin
+from webapp.models import Employee, Food, Order, OrderFoods
 
-# Register your models here.
+class EmployeeInLine(admin.StackedInline):
+    model = Employee
+    fields = ['phone']
+
+class EmployeeAdmin(UserAdmin):
+    inlines = [EmployeeInLine]
+
+class OrderFoodsInLine(admin.TabularInline):
+    model = OrderFoods
+    fields = ['food', 'amount']
+
+class OrderAdmin(admin.ModelAdmin):
+    model = Order
+    inlines = [OrderFoodsInLine]
+
+admin.site.unregister(User)
+admin.site.register(User, EmployeeAdmin)
+admin.site.register(Food)
+admin.site.register(Order, OrderAdmin)
+
